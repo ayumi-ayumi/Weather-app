2ef5f11515a23942707ac71b5a1cd0396e00af34
Ayumi <ayuayuayuayus@gmail.com>
2023-03-20 22:47:13 +0100

    added hanglechange function to get a value

--- src/Add_city.jsx
+++ src/Add_city.jsx
@@ -1,13 +1,33 @@
-function Add_city () {
+import { useState, useEffect } from 'react';
+
+function Add_city() {
+
+  const [addCityName, setAddCityName] = useState("")
+
+  function handleChange(event) {
+    setAddCityName(prevList => event.target.value)
+  }
+  
+  console.log(addCityName)
+
+
   return (
     <div>
-    <div className="m-5 ">
-    <form>
-      <input type="text" placeholder="Add a city" className="border-2 mr-4" autofocus></input>
-      <button type="submit" className="border-2 p-1">ADD</button>
-    </form>
-      
-    </div>
+      <div className="m-5 ">
+        <form>
+          <input
+            type="text"
+            placeholder="Add a city"
+            className="border-2 mr-4"
+            onChange={handleChange}
+            value={addCityName}
+            autofocus
+          ></input>
+          <button type="submit" className="border-2 p-1">ADD</button>
+        </form>
+        {addCityName && <p>{addCityName}</p>}
+
+      </div>
 
     </div>
   )
