928433b89f94308769e95db8c5effd7587248e0c
Ayumi <ayuayuayuayus@gmail.com>
2023-03-23 13:03:11 +0100

    edited sun time using dayjs

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -28,29 +28,9 @@ function Weather(props) {
   
   // Time
   const timezone = weather.timezone
-  const hour_timezone = timezone / 3600
-  // console.log(timezone)
   const date = dayjs.utc().utcOffset(timezone/60)
   const current_time = date.format("HH:mm")
   const hour = date.hour()
-  console.log(hour)
-  // console.log(date);
-  // console.log(now.utc().utcOffset(3600).format('h:mm A'));
-
-  // let date = new Date().toUTCString()
-  // let date = new Date((new Date().getTime()) + (timezone * 1000))
-  // console.log(date)
-  // const date = new Date().toUTCString() //UTC(GMT) time
-  // const date = utcTime.getTime() + (timezone/3600)*1000
-  // const date = utcTime
-  // const date = utcTime.getTime()
-  // console.log(utcTime.getTime())
-  // let date = new Date(dt*1000)
-  // console.log(date)
-  // const hour = date.getHours().toString().padStart(2, '0');
-  // const minute = date.getMinutes().toString().padStart(2, '0');
-  // const time = hour + ":" + minute
-
   // City name
   const city_name = weather.name
 
@@ -60,16 +40,14 @@ function Weather(props) {
   const min_temp = Math.round(weather.main.temp_min)
 
   // Sunrise time
-  const sunrise_time_date = new Date((weather.sys.sunrise + timezone) * 1000)
-  const sunrise_hour = sunrise_time_date.getHours().toString().padStart(2, '0');
-  const sunrise_minute = sunrise_time_date.getMinutes().toString().padStart(2, '0');
-  const sunrise_time = sunrise_hour + ":" + sunrise_minute
-
+  const sunrise_time_date = dayjs.unix(weather.sys.sunrise).utcOffset(timezone/60)
+  const sunrise_time = sunrise_time_date.format("HH:mm")
+  const sunrise_hour = sunrise_time_date.hour()
+  
   // Sunset time
-  const sunset_time_date = new Date((weather.sys.sunset + timezone) * 1000)
-  const sunset_hour = sunset_time_date.getHours().toString().padStart(2, '0');
-  const sunset_minute = sunset_time_date.getMinutes().toString().padStart(2, '0');
-  const sunset_time = sunset_hour + ":" + sunset_minute
+  const sunset_time_date = dayjs.unix(weather.sys.sunset).utcOffset(timezone/60)
+  const sunset_time = sunset_time_date.format("HH:mm")
+  const sunset_hour = sunset_time_date.hour()
 
   // Weather condition icon
   const icon_id = weather.weather[0].icon + "@2x.png"
